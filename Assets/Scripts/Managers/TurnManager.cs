0a8f1044837cd0847b74ddb9f8ccca306913b601
Caleb Doucet <Doucet.Caleb@gmail.com>
2016-03-17 22:34:45 -0600

    Add Assets and initial scene

---
+++ Assets/Scripts/Managers/TurnManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager : MonoBehaviour {
+    enum TurnPhase { start, income, upkeep, purchase, main, end };
+    enum MovementPhase { start, move, stop };
+    enum AttackPhase { start, calculateHit, calculateDamage, retaliationHit, retaliationDamage, end };
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
