c9d20d646319ea1be7e102f4df5fce68d4d1bd0f
Dane Christenson <danechristenson@gmail.com>
2016-03-26 14:13:12 -0600

    Added basic LevelManager and Start Scene

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name) {
+        Debug.Log("Level load requested for " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest() {
+        Application.Quit();
+    }
+}
