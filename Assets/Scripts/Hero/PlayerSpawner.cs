25a0f6dc4b287c1b12e9f73be569bed1dc286eea
Dane Christenson <danechristenson@gmail.com>
2016-03-26 11:16:25 -0600

    Added spawner script. (Note current prefabs seem to be broken with it.) added debug level

---
+++ Assets/Scripts/Hero/PlayerSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawner : MonoBehaviour {
+
+    public GameObject Avatar;
+    // Use this for initialization
+    void Start () {
+        //Note: default prefab with this doesn't initialize the avatar
+        Instantiate(Avatar);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        //Instantiate(Avatar);
+    }
+}
